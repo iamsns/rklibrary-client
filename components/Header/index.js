93ba617d5d9869794c87119bfe86e6236f132863
iamsns1997 <shiv.n@mamaearth.in>
2022-12-07 23:20:47 +0530

    replace anchor to link

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Wrapper from "./styled";
 import { useRouter } from "next/router";
 import AuthService from "../../services/authService";
+import Link from 'next/link'
 
 const Header = () => {
   const router = useRouter();
@@ -11,58 +12,75 @@ const Header = () => {
     localStorage.clear();
     router.push('/login')
   };
-  React.useEffect(()=>{
+  React.useEffect(() => {
     console.log('localStorage.getItem("isLoggedIn") === "true"', localStorage.getItem("isLoggedIn") === "true")
     setIsloggedIn(localStorage.getItem("isLoggedIn") === "true")
-  },[])
+  }, [])
   return (
     <>
       <Wrapper route={currentRoute}>
-        <a
-          className={currentRoute == "/" ? "active nav-items" : "nav-items"}
-          href="/"
-        >
-          Home
-        </a>
-        <a
-          className={
-            currentRoute == "/books" ? "active nav-items" : "nav-items"
-          }
-          href="/books"
-        >
-          Books
-        </a>
-        <a
-          className={
-            currentRoute == "/add-book" ? "active nav-items" : "nav-items"
-          }
-          href="/add-book"
-        >
-          Add Book
-        </a>
-        <div className="auth-item">
-          <a
-            className={
-              currentRoute == "/signup" ? "active nav-items" : "nav-items"
-            }
-            href="/signup"
+        <span className={currentRoute == "/" ? "active nav-items" : "nav-items"}>
+
+          <Link
+            className={currentRoute == "/" ? "active nav-items" : "nav-items"}
+            href="/"
           >
-            Signup
-          </a>
-          <a className="nav-items">Cart</a>
-          {!isLoggedIn ? (
-            <a
-              className={
-                currentRoute == "/login" ? "active nav-items" : "nav-items"
-              }
-              href="/login"
+            Home
+          </Link>
+        </span>
+        <span className={
+          currentRoute == "/books" ? "active nav-items" : "nav-items"
+        }>
+          <Link
+
+            href="/books"
+          >
+            Books
+          </Link>
+        </span>
+        <span className={
+          currentRoute == "/add-book" ? "active nav-items" : "nav-items"
+        }>
+          <Link
+
+            href="/add-book"
+          >
+            Add Book
+          </Link>
+        </span>
+        <div className="auth-item">
+          <span className={
+            currentRoute == "/signup" ? "active nav-items" : "nav-items"
+          }>
+            <Link
+
+              href="/signup"
             >
-              Login
-            </a>
+              Signup
+            </Link>
+          </span>
+          <span className="nav-items">
+            <Link href="#" >Cart</Link>
+          </span>
+          {!isLoggedIn ? (
+            <span className={
+              currentRoute == "/login" ? "active nav-items" : "nav-items"
+            }>
+              <Link
+
+                href="/login"
+              >
+                Login
+              </Link>
+            </span>
+
           ) : (
-            <a className="nav-items" onClick={handleLogout}>
-              Logout
-            </a>
+            <span className="nav-items" >
+              <Link className="nav-items" href="#" onClick={handleLogout}>
+                Logout
+              </Link>
+            </span>
+
           )}
         </div>
       </Wrapper>
