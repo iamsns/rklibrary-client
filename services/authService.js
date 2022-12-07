d84df9722f8660cad437445450aa65521b7de5a2
iamsns1997 <shiv.n@mamaearth.in>
2022-12-07 23:03:20 +0530

    Api url changed

--- services/authService.js
+++ services/authService.js
@@ -2,14 +2,12 @@ const axios = require('axios');
 const getJsonData = (endPoint) => {
   const token = localStorage.getItem("token")
   console.log("token", token)
-  return axios.get('http://localhost:4044'+endPoint, {'headers' : {token : token}})
-  // .get(process.env.API_URL+endPoint)
+  return axios.get("https://rk-library-backend.onrender.com"+endPoint, {'headers' : {token : token}})
 };
 const postJsonData = (endPoint, body) => {
   const token = localStorage.getItem("token")
-  console.log("token", token)
-  return axios.post('http://localhost:4044'+endPoint, body, {'headers' : {token : token}})
-    // .get(process.env.API_URL+endPoint)
+  console.log("process.env.API_URL", "https://rk-library-backend.onrender.com")
+  return axios.post("https://rk-library-backend.onrender.com"+endPoint, body, {'headers' : {token : token}})
 };
 const AuthService =  {
     getJsonData : getJsonData,
